885468a29191ed3f37dc5137e9f7a6d006df0499
hlusovich <bigklu95@gmail.com>
2021-06-22 23:48:11 +0300

    add slider on vanilla js

---
+++ app.js
@@ -0,0 +1,40 @@
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const pictures = mainSlide.querySelectorAll('div');
+const container = document.querySelector('.container');
+const height = container.clientHeight;
+let activeSlide = 0;
+sidebar.style.top = `-${(pictures.length - 1) * 100}vh`;
+upButton.addEventListener("click", () => {
+    changeSlide('up');
+    mainSlide.style.transform = `translateY(-${activeSlide*height}px)`;
+});
+downButton.addEventListener("click", () => {
+    changeSlide('down');
+    mainSlide.style.transform = `translateY(-${activeSlide*height}px)`;
+});
+
+function changeSlide(direction) {
+    switch (direction) {
+        case "up":
+            activeSlide++;
+            break;
+        case "down":
+            activeSlide--;
+            break;
+        default:
+            return activeSlide;
+    }
+    if (activeSlide >= pictures.length) {
+        activeSlide = 0;
+        return
+    }
+    if (activeSlide < 0) {
+        activeSlide = pictures.length - 1;
+        return
+    }
+
+
+}
